4de30107f439ebddc27539990934b41980af73fe
c9s <yoanlin93@gmail.com>
2015-09-14 13:38:14 +0800

    Improve publish script

--- publish.sh
+++ publish.sh
@@ -13,12 +13,12 @@ sculpin generate --env=prod
 git stash
 git checkout gh-pages
 
-cp -R output_prod/* .
+rsync -av output_prod/ ./
 rm -rf output_*
 
 git add *
 git commit -m "$1"
-git push origin --all
+git push origin gh-pages
 
 git checkout master
 git stash pop
